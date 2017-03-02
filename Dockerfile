388e047f31a8d993e9e24339c283707af2569cff
Patrick Seemann <patsee14@gmail.com>
2017-03-02 17:55:58 +0100

    Build camlistore with sqlite support

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,14 @@ MAINTAINER theorician
 
 ENV UID=991 GID=991
 
-RUN apk update && apk add --no-cache git sqlite-dev su-exec
+RUN apk update && apk add --no-cache gcc libc-dev git sqlite-dev su-exec
 
 ADD build.sh /build.sh
 RUN /build.sh
+RUN apk del gcc git
 
 VOLUME ["/config", "/store"]
+
 
 ADD run.sh /run.sh
 CMD /run.sh
--- build.sh
+++ build.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
-die () {
-  echo >&2 "$@"
-  exit 1
-}
-
-apk add --no-cache gcc
-
 git clone https://github.com/camlistore/camlistore.git /tmp/camlistore
 cd /tmp/camlistore
-go run make.go
+go run make.go --sqlite=true
 cp -r bin/* /usr/local/bin/
 rm /usr/local/bin/README
 rm -rf /tmp/camlistore
-
 rm /build.sh
-
-apk del gcc
 
 ln -s /home/camlistore/var/camlistore /store
 ln -s /home/camlistore/.config/camlistore /config
